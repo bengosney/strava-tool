6bfc82896f67ba6efd2b4d1a4b8dc8dbe3f3c846
Ben Gosney <bengosney@googlemail.com>
2024-05-27 11:31:06 +0100

    fix(chrome): run chrome without a sandbox

--- auto-kudos.js
+++ auto-kudos.js
@@ -2,7 +2,7 @@ import puppeteer from "puppeteer";
 import process from 'node:process';
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args:['--no-sandbox','--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.setViewport({ width: 1280, height: 2000 });
   await page.goto("https://www.strava.com/login");
