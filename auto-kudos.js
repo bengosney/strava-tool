9b967e6adf28e37bc67abbc81b715c1de60c5d14
Ben Gosney <bengosney@googlemail.com>
2024-05-27 11:41:47 +0100

    feat(config): swap to a config file
    BREAKING CHANGE:

--- auto-kudos.js
+++ auto-kudos.js
@@ -1,5 +1,5 @@
 import puppeteer from "puppeteer";
-import process from 'node:process';
+import config from './config.js';
 
 (async () => {
   const browser = await puppeteer.launch({args:['--no-sandbox','--disable-setuid-sandbox']});
@@ -7,14 +7,14 @@ import process from 'node:process';
   await page.setViewport({ width: 1280, height: 2000 });
   await page.goto("https://www.strava.com/login");
 
-  await page.type("#email", process.env.STRAVA_USERNAME);
-  await page.type("#password", process.env.STRAVA_PASSWORD);
+  await page.type("#email", config.strava.username);
+  await page.type("#password", config.strava.password);
   await Promise.all([
     page.click("#login-button"),
     page.waitForNavigation({ waitUntil: "networkidle2" }),
   ]);
 
-  await page.goto(`https://www.strava.com/clubs/${process.env.STRAVA_CLUB_ID}/recent_activity`);
+  await page.goto(`https://www.strava.com/clubs/${config.strava.club_id}/recent_activity`);
   await page.$$eval(
     'button[data-testid="kudos_button"]:has([data-testid="unfilled_kudos"])',
     (buttons) => buttons.forEach((button) => button.click())
