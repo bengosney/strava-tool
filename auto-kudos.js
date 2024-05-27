1c75aa614c0b3d039d526df8818f50903e8eec49
Ben Gosney <bengosney@googlemail.com>
2024-05-27 16:07:35 +0100

    feat(logging): add time stamps to the logs

--- auto-kudos.js
+++ auto-kudos.js
@@ -1,14 +1,16 @@
 import puppeteer from "puppeteer";
-import config from './config.js';
+import config from "./config.js";
+
+const log = (message, ...options) => console.log(`[${new Date().toISOString()}] ${message}`, ...options);
 
 (async () => {
-  console.log("Starting auto-kudos script");
-  const browser = await puppeteer.launch({args:['--no-sandbox','--disable-setuid-sandbox']});
+  log("Starting auto-kudos script");
+  const browser = await puppeteer.launch({ args: ["--no-sandbox", "--disable-setuid-sandbox"] });
   const page = await browser.newPage();
   await page.setViewport({ width: 1280, height: 2000 });
   await page.goto("https://www.strava.com/login");
 
-  console.log("Logging in");
+  log("Logging in");
   await page.type("#email", config.strava.username);
   await page.type("#password", config.strava.password);
   await Promise.all([
@@ -16,16 +18,15 @@ import config from './config.js';
     page.waitForNavigation({ waitUntil: "networkidle2" }),
   ]);
 
-  console.log("Navigating to club page", config.strava.club_id);
+  log("Navigating to club page", config.strava.club_id);
   await page.goto(`https://www.strava.com/clubs/${config.strava.club_id}/recent_activity`);
+
+  log("Add kudos all activities");
   await page.$$eval(
     'button[data-testid="kudos_button"]:has([data-testid="unfilled_kudos"])',
-    (buttons) => {
-      console.log(`Kudos buttons found: ${buttons.length}`);
-      buttons.forEach((button) => button.click());
-    }
+    (buttons) => buttons.forEach((button) => button.click())
   );
 
   await browser.close();
-  console.log("Auto-kudos script completed");
+  log("Auto-kudos script completed");
 })();
